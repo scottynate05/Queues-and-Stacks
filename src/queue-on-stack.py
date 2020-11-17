36f30740ca68cfa74e7ad37c122f9252e299ac99
Nathan Brown <scottynate05@yahoo.com>
2020-11-16 19:14:46 -0800

    stack setup

--- src/queue-on-stack.py
+++ src/queue-on-stack.py
@@ -26,3 +26,35 @@
 
 # Return an array composed of the results of each "pop" operation that is performed.
 
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+
+def queueOnStacks(requests):
+    left = Stack()
+    right = Stack()
+
+    def insert(x):
+        ...
+
+    def remove():
+        ...
+
+    ans = []
+    for request in requests:
+        req = request.split(" ")
+        if req[0] == 'push':
+            insert(int(req[1]))
+        else:
+            ans.append(remove())
+    return ans
